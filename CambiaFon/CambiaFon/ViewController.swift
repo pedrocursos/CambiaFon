9afeed8d93f71718afd765025a5386cb564b3057
Pedro Cursos <pedrocursos@icloud.com>
2018-05-26 14:03:23 +0200

    Añadir interruptor para cambiar el color de fondo de la pantalla.

--- CambiaFon/CambiaFon/ViewController.swift
+++ CambiaFon/CambiaFon/ViewController.swift
@@ -1,15 +1,17 @@
-//
-//  ViewController.swift
-//  CambiaFon
-//
-//  Created by Cursos on 26/5/18.
-//  Copyright © 2018 Pedro Hernández. All rights reserved.
-//
+//  CambiaFon app by Pedro Hernández
+//  Copyright 2018 Pedro Hernández.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Propiedad global
+    
+    /// Interruptor que cambia el color de la pantalla. El color de la pantalla es amarillo si está en posición On o gris oscuro si está en Off.
+    @IBOutlet weak var interruptor: UISwitch!
+    
+    // MARK: - Ciclo de vida
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Acción
+    
+    /**
+     Cambia el color de la pantalla y modifica el tamaño del interruptor.
+     
+     - parameters:
+        - sender: el interruptor
+     */
+    @IBAction func interruptorPulsado(_ sender: UISwitch) {
+        view.backgroundColor = sender.isOn == true ? .yellow : .darkGray
+        var escalado: CGAffineTransform = CGAffineTransform(scaleX: 2, y: 2)
+        if interruptor.isOn {
+            escalado = .identity
+        }
+        sender.transform = escalado
+    }
+    
 }
 
